679effde4fc01a2aa0cb2b99c8b15a0ee95e1f2b
Stephen Webber <offgridauthor@gmail.com>
2021-12-30 19:05:54 -0800

    set claim conditions

---
+++ scripts/4-set-claim-condition.js
@@ -0,0 +1,23 @@
+import sdk from "./1-initialize-sdk.js";
+
+const bundleDrop = sdk.getBundleDropModule(
+  "0x6e8Df43d8113ba09c25A535AA16bBEFf91371569"
+);
+
+(async () => {
+  try {
+    const claimConditionFactory = bundleDrop.getClaimConditionFactory();
+    claimConditionFactory.newClaimPhase({
+      startTime: new Date(),
+      maxQuantity: 50_000,
+      maxQuantityPerTransaction: 1,
+    });
+    await bundleDrop.setClaimCondition(0, claimConditionFactory);
+    console.log(
+      "✅ Successfully set claim condition on bundle drop:",
+      bundleDrop.address
+    );
+  } catch (error) {
+    console.error("Failed to set claim condition", error);
+  }
+})();
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
 import { useEffect, useMemo, useState } from "react";
-
+import { ThirdwebSDK } from "@3rdweb/sdk";
 import { useWeb3 } from "@3rdweb/hooks";
+
+const sdk = new ThirdwebSDK("rinkeby");
+const bundleDropModule = sdk.getBundleDropModule(
+  "0x6e8Df43d8113ba09c25A535AA16bBEFf91371569"
+);
 
 const App = () => {
   // thirdweb's hook for connect wallet
   const { connectWallet, address, error, provider } = useWeb3();
   console.log("👋 Address:", address);
+
+  
+
+  const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+
+  useEffect(() => {
+    if (!address) {
+      return;
+    }
+
+    return bundleDropModule
+      .balanceOf(address, "0")
+      .then((balance) => {
+        if (balance.gt(0)) {
+          setHasClaimedNFT(true);
+          console.log("🌟 this user has a membership NFT!");
+        } else {
+          setHasClaimedNFT(false);
+          console.log("😭 this user doesn't have a membership NFT.");
+        }
+      })
+      .catch((error) => {
+        setHasClaimedNFT(false);
+        console.error("failed to nft balance", error);
+      });
+  }, [address]);
 
   if (!address) {
     return (
@@ -21,7 +52,8 @@ const App = () => {
   return (
     <div className="landing">
       <h1>👀 wallet connected</h1>
-    </div>);
+    </div>
+  );
 };
 
 export default App;
